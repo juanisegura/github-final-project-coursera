9a47bfe51a8cf87352e54e2ebd7b67cfcd6fd268
juanisegura <jigsegura03@gmail.com>
2025-03-15 00:02:59 -0300

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
 # Do not use this in production. Sample purpose only.
 # Author: Upkar Lidder (IBM)
 # Additional Authors:
-# <your GitHub username>
+# Juan Ignacio Segura
 echo "Enter the principal:"
 read p
 echo "Enter rate of interest per year:"
